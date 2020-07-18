21097e9488c0654eb397e47dc871eaeb86ca8e27
BrennenMcGill <31702142+BrennenMcGill@users.noreply.github.com>
2020-07-18 16:33:22 -0600

    made the list on the left side of /notes show and fixed css

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const db = require('./db/db');
+const fs = require('fs');
 
-
+// Sets up the Express app to handle data parsing
+app.use('/static', express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
@@ -11,6 +16,11 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => {
+  res.json(db);
+});
+
 
 app.listen(3001, () => {
   console.log(`API server now on port 3001!`);
