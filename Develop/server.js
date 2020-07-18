e09b1957cb833abb820dcb6de90874aea5e8930a
BrennenMcGill <31702142+BrennenMcGill@users.noreply.github.com>
2020-07-18 14:32:15 -0600

    moved server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-  
 
-  app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, './public/index.html'));
-  });
 
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-  app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
